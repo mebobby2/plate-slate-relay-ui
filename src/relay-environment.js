fd9d739ceb76d2249290aac776deb58c5c8d3904
Bobby <bobby.lei@vodafone.com>
2020-06-17 21:38:43 +1200

    Patch absinthe-socket with Relay 9

--- src/relay-environment.js
+++ src/relay-environment.js
@@ -1,12 +1,25 @@
 import { createFetcher, createSubscriber } from '@absinthe/socket-relay'
-import { Environment, Network, RecordSource, Store } from 'relay-runtime'
+import { Environment, Network, RecordSource, Store, Observable } from 'relay-runtime'
 
 import absintheSocket from './absinthe-socket'
+
+const legacySubscribe = createSubscriber(absintheSocket);
+
+// @absinthe/socket-relay is outdated so wrap it with a fix
+const subscribe = (request, variables, cacheConfig) => {
+  return Observable.create(sink => {
+    legacySubscribe(request, variables, cacheConfig, {
+      onNext: sink.next,
+      onError: sink.error,
+      onCompleted: sink.complete
+    });
+  });
+};
 
 export default new Environment({
   network: Network.create(
     createFetcher(absintheSocket),
-    createSubscriber(absintheSocket),
+    subscribe,
   ),
   store: new Store(new RecordSource()),
 })
