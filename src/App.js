d4e6924110268d0e9b5565c13f4348512844bac2
Bobby <bobby.lei@vodafone.com>
2020-06-17 21:32:41 +1200

    Add subscriptions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { QueryRenderer, graphql } from 'react-relay'
 import environment from './relay-environment'
+import NewMenuItemSubscription from './subscriptions/NewMenuItemSubscription';
 
 const query = graphql`
   query AppQuery {
@@ -11,6 +12,10 @@ const query = graphql`
   }
 `
 class App extends Component {
+  componentDidMount() {
+    NewMenuItemSubscription();
+  }
+
   renderMenuItem(menuItem) {
     return <li key={menuItem.id}>{menuItem.name}</li>
   }
---
+++ src/subscriptions/NewMenuItemSubscription.js
@@ -0,0 +1,30 @@
+import { graphql, requestSubscription } from 'react-relay'
+import environment from '../relay-environment'
+
+const newMenuItemSubscription = graphql`
+  subscription NewMenuItemSubscription {
+    newMenuItem {
+      id
+      name
+    }
+  }
+`
+export default () => {
+  const subscriptionConfig = {
+    subscription: newMenuItemSubscription,
+    variables: {},
+    updater: proxyStore => {
+      // Get the new menu item
+      const newMenuItem = proxyStore.getRootField('newMenuItem')
+
+      // Get existing menu items
+      const root = proxyStore.getRoot()
+      const menuItems = root.getLinkedRecords('menuItems')
+
+      // Prepend the new menu item
+      root.setLinkedRecords([newMenuItem, ...menuItems], 'menuItems')
+    },
+    onError: error => console.log(`An error occured:`, error),
+  }
+  requestSubscription(environment, subscriptionConfig)
+}
